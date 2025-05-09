be6a1b50679df1755aaeae9e38088e0bfb20d03b
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-05-09 23:55:01 +0530

    collections added.

---
+++ src/vt/utils/commons/commons/collections/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+Reusable common utilities, interfaces and implementations for python projects related to collections.
+"""
+
+from vt.utils.commons.commons.collections.utils import *
