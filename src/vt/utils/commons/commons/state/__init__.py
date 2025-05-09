563f55eda49474a00947d103ba5fb1b61b7e5abe
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-05-09 23:55:13 +0530

    state added.

---
+++ src/vt/utils/commons/commons/state/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+Reusable common utilities, interfaces and implementations for python projects related to states and state variables.
+"""
+
+from vt.utils.commons.commons.state.done import DoneEnquirer, DoneMarker, DoneVisitor, DelegatingDoneVisitor
