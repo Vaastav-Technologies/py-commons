f72500c36e7404d6562ac455be8e5e0c2c43acea
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-05-20 22:17:43 +0530

    `RootDirOp` interface and one implementation added.

--- src/vt/utils/commons/commons/op/base.py
+++ src/vt/utils/commons/commons/op/base.py
@@ -4,8 +4,10 @@
 """
 Reusable interfaces for python projects related to operations.
 """
+from __future__ import annotations
 from abc import abstractmethod
-from typing import Protocol
+from pathlib import Path
+from typing import Protocol, override
 
 
 class ReversibleOp(Protocol):
@@ -20,3 +22,78 @@ class ReversibleOp(Protocol):
         :return: whether current operation is operating in the reverse mode.
         """
         ...
+
+
+class RootDirOp(Protocol):
+    """
+    Perform operations on the ``root_dir``.
+    """
+
+    @property
+    @abstractmethod
+    def root_dir(self) -> Path:
+        ...
+
+    @staticmethod
+    def strictly_one_required(root_dir: Path | None = None, root_dir_op: RootDirOp | None = None, *,
+                              root_dir_str: str = 'root_dir', root_dir_op_str: str = 'root_dir_op'):
+        """
+        Convenience method to raise ``ValueError`` when both ``root_dir`` and ``root_dir_op`` are supplied.
+
+        Examples:
+
+          * OK: only root-dir supplied:
+
+            >>> RootDirOp.strictly_one_required(Path.cwd())
+
+          * OK: only root-dir-op supplied:
+
+            >>> RootDirOp.strictly_one_required(root_dir_op=RootDirOp.from_path(Path('tmp')))
+
+          * At least one of ``root_dir`` or ``root_dir_op`` must be provided:
+
+            >>> RootDirOp.strictly_one_required(None, None)
+            Traceback (most recent call last):
+            ValueError: Either root_dir or root_dir_op is required.
+
+          * Both ``root_dir`` or ``root_dir_op`` cannot be provided:
+
+            >>> RootDirOp.strictly_one_required(root_dir=Path.cwd(), root_dir_op=RootDirOp.from_path(Path('tmp')))
+            Traceback (most recent call last):
+            ValueError: root_dir and root_dir_op are not allowed together.
+
+        :param root_dir: path to the root directory.
+        :param root_dir_op: object that has path to the root directory.
+        :param root_dir_str: variable name string for overriding the default ``root_op`` variable name in error
+            messages.
+        :param root_dir_op_str: variable name string for overriding the default ``root_dir_op`` variable name in error
+            messages.
+        :raises ValueError: when both ``root_dir`` and ``root_dir_op`` are supplied.
+        """
+        if root_dir is None and root_dir_op is None:
+            raise ValueError(f"Either {root_dir_str} or {root_dir_op_str} is required.")
+        if root_dir and root_dir_op:
+            raise ValueError(f"{root_dir_str} and {root_dir_op_str} are not allowed together.")
+
+    @staticmethod
+    def from_path(root_dir: Path = Path.cwd()) -> CWDRootDirOp:
+        """
+        :param root_dir: path to root-dir.
+        :return: a root dir operation for the supplied path.
+        """
+        return CWDRootDirOp(root_dir)
+
+
+class CWDRootDirOp(RootDirOp):
+    def __init__(self, root_dir=Path.cwd()):
+        """
+        Perform operations on the root_dir.
+
+        :param root_dir: the path to the root directory.
+        """
+        self._root_dir = root_dir
+
+    @override
+    @property
+    def root_dir(self) -> Path:
+        return self._root_dir
