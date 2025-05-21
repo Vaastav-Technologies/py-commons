26f3cd6903679d8d45dde1bcaf33ad478288949e
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-05-21 22:51:07 +0530

    Reformatted file for better clarity.

--- src/vt/utils/commons/commons/core_py/base.py
+++ src/vt/utils/commons/commons/core_py/base.py
@@ -7,14 +7,14 @@ Reusable interfaces and sentinel objects related to core python.
 
 from vt.utils.commons.commons.core_py._base import _MISSING
 
-MISSING = _MISSING()
+MISSING_TYPE = _MISSING
+"""
+Sentinel value type useful for type hinting.
+"""
+
+MISSING = MISSING_TYPE()
 """
 Sentinel to represent a missing value. Can be used:
 
 * as default value for a parameter which has ``None`` as a valid value.
-"""
-
-MISSING_TYPE = _MISSING
-"""
-Sentinel value type useful for type hinting.
 """
