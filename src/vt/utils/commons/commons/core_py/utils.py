752f53c49618fee0a500b22027afb7629aae56fc
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-05-22 10:13:44 +0530

    added `alt_if_unset()`.

--- src/vt/utils/commons/commons/core_py/utils.py
+++ src/vt/utils/commons/commons/core_py/utils.py
@@ -4,9 +4,9 @@
 """
 Reusable utilities related to core python.
 """
-
+from collections.abc import Callable
 from typing import Any, cast
-from vt.utils.commons.commons.core_py.base import MISSING, MISSING_TYPE, UNSET
+from vt.utils.commons.commons.core_py.base import MISSING, MISSING_TYPE, UNSET, UNSET_TYPE
 
 
 def is_missing[T](obj: T) -> bool:
@@ -100,7 +100,28 @@ def is_unset[T](obj: T) -> bool:
     :param obj: object to be tested whether it was supplied by caller or not.
     :return: ``True`` if the ``obj`` is deliberatley unset by the caller, ``False`` otherwise.
     """
-    return obj == UNSET
+    return is_missing(obj)
+
+
+def _alt_if_predicate_true[T, U](obj: Any | U, alt: T, predicate: Callable[[Any | U], bool]) -> T:
+    """
+    Get an alternate object ``alt`` if the queried object ``obj`` is ``MISSING``, i.e. it is not supplied by the caller.
+
+    Note::
+
+        Returned value is always of the type of alt object.
+
+    :param obj: object to be tested whether it was fulfills the ``predicate`` or not.
+    :param predicate: A predicate that ``obj`` needs to fulfill to be returned from this method.
+    :param alt: alternate object to be returned if ``obj`` does not fulfill the ``predicate``.
+    :return: ``obj`` if it fulfills the ``predicate`` else ``alt``.
+    """
+    if predicate(obj):
+        return alt
+    if type(obj) != type(alt):
+        raise TypeError(f"Unexpected type: `obj` and `alt` must be of the same type. type(obj): {type(obj)}, "
+                        f"type(alt): {type(alt)}")
+    return alt if is_missing(obj) else cast(T, obj)
 
 
 def alt_if_missing[T](obj: Any | MISSING_TYPE, alt: T) -> T:
@@ -111,7 +132,7 @@ def alt_if_missing[T](obj: Any | MISSING_TYPE, alt: T) -> T:
 
         Returned value is always of the type of alt object.
 
-    Example:
+    Examples:
 
     * Main object ``obj`` is returned if it is not ``MISSING``, i.e. it was supplied by the caller. Also, the returned
       object ``obj`` is of the type of alternative ``alt`` object, test for falsy ``obj`` objects:
@@ -164,12 +185,72 @@ def alt_if_missing[T](obj: Any | MISSING_TYPE, alt: T) -> T:
     :param alt: alternate object to be returned if ``obj`` was not supplied by the caller.
     :return: ``obj`` if it was supplied by the caller else ``alt``.
     """
-    if is_missing(obj):
-        return alt
-    if type(obj) != type(alt):
-        raise TypeError(f"Unexpected type: `obj` and `alt` must be of the same type. type(obj): {type(obj)}, "
-                        f"type(alt): {type(alt)}")
-    return alt if is_missing(obj) else cast(T, obj)
+    return _alt_if_predicate_true(obj, alt, is_missing)
+
+
+def alt_if_unset[T](obj: Any | UNSET_TYPE, alt: T) -> T:
+    """
+    Get an alternate object ``alt`` if the queried object ``obj`` is ``UNSET``, i.e. it is deliberately unset by
+    the caller.
+
+    Note::
+
+        Returned value is always of the type of alt object.
+
+    Examples:
+
+    * Main object ``obj`` is returned if it is not ``UNSET``, i.e. it was not deliberately unset by the caller. Also,
+      the returned object ``obj`` is of the type of alternative ``alt`` object, test for falsy ``obj`` objects:
+
+    >>> assert alt_if_unset(None, None) is None
+    >>> assert alt_if_unset(0, 2) == 0
+    >>> assert alt_if_unset(0.0, 1.3) == 0.0
+    >>> assert alt_if_unset('', 'z') == ''
+    >>> assert alt_if_unset([], [1, 2, 3]) == []
+    >>> assert alt_if_unset({}, {'a': 1, 'b': 2}) == {}
+    >>> assert alt_if_unset(set(), {1, 2, 3}) == set()
+    >>> assert alt_if_unset(0j, 1+2j) == 0j
+
+    * Main object ``obj`` is returned if it is not ``UNSET``, i.e. it was not deliberately unset by the caller. Also,
+      the returned object ``obj`` is of the type of alternative ``alt`` object, test for truthy ``obj`` objects:
+
+    >>> assert alt_if_unset('a', 'null') == 'a'
+    >>> assert alt_if_unset(-1, 2) == -1
+    >>> assert alt_if_unset(0.9, 1.3) == 0.9
+    >>> assert alt_if_unset('jo', 'z') == 'jo'
+    >>> assert alt_if_unset([9, 8, 7], [1, 2, 3]) == [9, 8, 7]
+    >>> assert alt_if_unset({'z': 10, 'y': 9, 'x': 8}, {'a': 1, 'b': 2}) == {'z': 10, 'y': 9, 'x': 8}
+    >>> assert alt_if_unset({0, 9, 8}, {1, 2, 3}) == {0, 9, 8}
+    >>> assert alt_if_unset(1+0j, 1+2j) == 1+0j
+
+    * Alternate object ``alt`` is returned when main object ``obj`` is ``UNSET``, i.e. it was deliberately unset by
+      the caller. Also, the returned object ``alt`` is of the type of alternative ``alt`` object:
+
+    >>> assert alt_if_unset(UNSET, None) is None
+    >>> assert alt_if_unset(UNSET, 0) == 0
+    >>> assert alt_if_unset(UNSET, 0.0) == 0,0
+    >>> assert alt_if_unset(UNSET, '') == ''
+    >>> assert alt_if_unset(UNSET, []) == []
+    >>> assert alt_if_unset(UNSET, {}) == {}
+    >>> assert alt_if_unset(UNSET, set()) == set()
+    >>> assert alt_if_unset(UNSET, 0j) == 0j
+
+    * Errs if main object ``obj`` is not ``UNSET`` and hence, is supplied by the caller, but its type is different
+      from the type of the alternative ``alt`` object:
+
+    >>> alt_if_unset('a', 2)
+    Traceback (most recent call last):
+    TypeError: Unexpected type: `obj` and `alt` must be of the same type. type(obj): <class 'str'>, type(alt): <class 'int'>
+
+    >>> alt_if_unset([], (2, 3, 4))
+    Traceback (most recent call last):
+    TypeError: Unexpected type: `obj` and `alt` must be of the same type. type(obj): <class 'list'>, type(alt): <class 'tuple'>
+
+    :param obj: object to be tested whether it was deliberately unset by the caller or not.
+    :param alt: alternate object to be returned if ``obj`` was not supplied by the caller.
+    :return: ``alt`` if ``obj`` was deliberately unset by the caller, else ``obj``.
+    """
+    return _alt_if_predicate_true(obj, alt, is_unset)
 
 
 def is_ellipses(obj: Any) -> bool:
@@ -182,11 +263,68 @@ def is_ellipses(obj: Any) -> bool:
 
 def alt_if_ellipses[T](obj, alt: T) -> T:
     """
-    :param obj: object to be tested whether it was supplied by caller or not.
-    :param alt: alternate object to be returned if ``obj`` was not supplied by the caller.
-    :return: ``obj`` if it was supplied by the caller else ``alt``.
+    Get an alternate object ``alt`` if the queried object ``obj`` is ``...``, i.e. it is not supplied by the caller or
+    is deliberatey kept ``...`` by the caller.
+
+    Note::
+
+        Returned value is always of the type of alt object.
+
+    Examples:
+
+    * Main object ``obj`` is returned if it is not ``...``, i.e. it was supplied by the caller. Also, the returned
+      object ``obj`` is of the type of alternative ``alt`` object, test for falsy ``obj`` objects:
+
+    >>> assert alt_if_ellipses(None, None) is None
+    >>> assert alt_if_ellipses(0, 2) == 0
+    >>> assert alt_if_ellipses(0.0, 1.3) == 0.0
+    >>> assert alt_if_ellipses('', 'z') == ''
+    >>> assert alt_if_ellipses([], [1, 2, 3]) == []
+    >>> assert alt_if_ellipses({}, {'a': 1, 'b': 2}) == {}
+    >>> assert alt_if_ellipses(set(), {1, 2, 3}) == set()
+    >>> assert alt_if_ellipses(0j, 1+2j) == 0j
+
+    * Main object ``obj`` is returned if it is not ``...``, i.e. it was supplied by the caller. Also, the returned
+      object ``obj`` is of the type of alternative ``alt`` object, test for truthy ``obj`` objects:
+
+    >>> assert alt_if_ellipses('a', 'null') == 'a'
+    >>> assert alt_if_ellipses(-1, 2) == -1
+    >>> assert alt_if_ellipses(0.9, 1.3) == 0.9
+    >>> assert alt_if_ellipses('jo', 'z') == 'jo'
+    >>> assert alt_if_ellipses([9, 8, 7], [1, 2, 3]) == [9, 8, 7]
+    >>> assert alt_if_ellipses({'z': 10, 'y': 9, 'x': 8}, {'a': 1, 'b': 2}) == {'z': 10, 'y': 9, 'x': 8}
+    >>> assert alt_if_ellipses({0, 9, 8}, {1, 2, 3}) == {0, 9, 8}
+    >>> assert alt_if_ellipses(1+0j, 1+2j) == 1+0j
+
+    * Alternate object ``alt`` is returned when main object ``obj`` is ``...``, i.e. it was not supplied by
+      the caller or is deliberately kept ``...`` by the caller. Also, the returned object ``alt`` is of the type of
+      alternative ``alt`` object:
+
+    >>> assert alt_if_ellipses(..., None) is None
+    >>> assert alt_if_ellipses(..., 0) == 0
+    >>> assert alt_if_ellipses(..., 0.0) == 0,0
+    >>> assert alt_if_ellipses(..., '') == ''
+    >>> assert alt_if_ellipses(..., []) == []
+    >>> assert alt_if_ellipses(..., {}) == {}
+    >>> assert alt_if_ellipses(..., set()) == set()
+    >>> assert alt_if_ellipses(..., 0j) == 0j
+
+    * Errs if main object ``obj`` is not ``...`` and hence, is supplied by the caller, but its type is different
+      from the type of the alternative ``alt`` object:
+
+    >>> alt_if_ellipses('a', 2)
+    Traceback (most recent call last):
+    TypeError: Unexpected type: `obj` and `alt` must be of the same type. type(obj): <class 'str'>, type(alt): <class 'int'>
+
+    >>> alt_if_ellipses([], (2, 3, 4))
+    Traceback (most recent call last):
+    TypeError: Unexpected type: `obj` and `alt` must be of the same type. type(obj): <class 'list'>, type(alt): <class 'tuple'>
+
+    :param obj: object to be tested whether it was supplied as ellipses by caller or not.
+    :param alt: alternate object to be returned if ``obj`` is supplied as ellipses by the caller.
+    :return: ``obj`` if it was supplied as ellipses by the caller, else ``alt``.
     """
-    return alt if is_ellipses(obj) else cast(T, obj)
+    return _alt_if_predicate_true(obj, alt, is_ellipses)
 
 
 def fallback_on_none[T](value:T | None, default_val: T | None) -> T | None:
